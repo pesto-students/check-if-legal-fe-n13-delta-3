ec07802424e31566259b748c9b80dc7ffc3958e2
Kunal Gosrani <iamkunalgosrani@gmail.com>
2022-02-07 18:45:57 +0530

    setup: configs and env variables

--- src/routes/AppRoutes.tsx
+++ src/routes/AppRoutes.tsx
@@ -1,6 +1,6 @@
 import { FC } from "react"
 import { useRoutes } from "react-router-dom"
-import { publicRoutes } from "./publicRoutes"
+import { publicRoutes } from "./public.routes"
 
 export const AppRoutes: FC = () => {
 	const routes = publicRoutes
