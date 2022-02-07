6688996387e0c6cc202f756ddc2104268a7a5bca
Kunal Gosrani <iamkunalgosrani@gmail.com>
2022-02-07 15:08:05 +0530

    setup: fresh create-react-app

---
+++ src/App.tsx
@@ -0,0 +1,9 @@
+function App() {
+  return (
+    <div>
+      <div>Check If Legal</div>
+    </div>
+  );
+}
+
+export default App;
--- src/redux/interfaces/userinterface.ts
+++
@@ -1,8 +0,0 @@
-import { SET_USER_DATA, CLEAR_USER_DATA } from '../constants';
-
-interface userState {
-  type: typeof SET_USER_DATA | typeof CLEAR_USER_DATA;
-  payload: {};
-}
-
-export type userTypes = userState;
