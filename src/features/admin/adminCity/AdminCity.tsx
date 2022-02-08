e17e8fcb86d79e5882a85fe32ebdf232c94782bf
Kunal Gosrani <iamkunalgosrani@gmail.com>
2022-02-09 03:47:33 +0530

    feat: admin city add drawer

--- src/features/admin/adminCity/AdminCity.tsx
+++ src/features/admin/adminCity/AdminCity.tsx
@@ -1,26 +1,55 @@
-import { Box } from "@chakra-ui/react"
-import { FC, useEffect } from "react"
+import { Box, Button, useDisclosure } from "@chakra-ui/react"
+import { FC, useCallback, useEffect } from "react"
 import { SidebarLayout } from "../../../components/sidebar/SidebarLayout"
 import { CenteredSpinner } from "../../../components/ui/CenterSpinner"
 import { AuthRole } from "../../../utils/enums"
 import { useFetchCities } from "../../city/useFetchCities"
+import { useFetchStates } from "../../state/useFetchStates"
 import { useAdminAuth } from "../useAdminAuth"
+import { CityAddDrawer } from "./cityAdd/CityAddDrawer"
 import { CityListView } from "./CityListView"
 
 export const AdminCity: FC = () => {
 	useAdminAuth()
+	const { states, fetchStates } = useFetchStates()
 	const { cities, isLoading, fetchCities } = useFetchCities()
+	const cityAddDrawerDisclosure = useDisclosure()
 
 	useEffect(() => {
+		fetchStates()
+		fetchCities()
+	}, [fetchStates, fetchCities])
+
+	const onCityAddSuccess = useCallback(() => {
 		fetchCities()
 	}, [fetchCities])
 
 	return (
 		<SidebarLayout role={AuthRole.ADMIN} headingText="Cities" p={2}>
 			<Box my={4}>
+				<Box my={4}>
+					<Button
+						size={"sm"}
+						colorScheme="blue"
+						onClick={() => cityAddDrawerDisclosure.onOpen()}
+					>
+						Add City
+					</Button>
+				</Box>
+
+				{/* City List */}
 				{isLoading && <CenteredSpinner />}
 				{cities && <CityListView cities={cities} />}
 			</Box>
+
+			{/* Drawers */}
+			{states && (
+				<CityAddDrawer
+					states={states}
+					onSuccess={onCityAddSuccess}
+					{...cityAddDrawerDisclosure}
+				/>
+			)}
 		</SidebarLayout>
 	)
 }
---
+++ src/features/admin/adminCity/cityAdd/CityAddDrawer.tsx
@@ -0,0 +1,72 @@
+import { FormControl, Input, Stack } from "@chakra-ui/react"
+import { ComponentProps, FC, useState } from "react"
+import { useForm } from "react-hook-form"
+import { DrawerForm } from "../../../../components/ui/DrawerForm"
+import { ErrorText } from "../../../../components/ui/ErrorText"
+import { InputLabel } from "../../../../components/ui/InputLabel"
+import { IState } from "../../../state/IState"
+import { cityAddApi } from "./cityAddApi"
+import { Select } from "chakra-react-select"
+import { useAdminAuth } from "../../useAdminAuth"
+
+type IProps = Omit<ComponentProps<typeof DrawerForm>, "children"> & {
+	states: IState[]
+	onSuccess?: () => void
+}
+
+interface IFormData {
+	name: string
+	stateId: number
+}
+
+export const CityAddDrawer: FC<IProps> = ({ states, onSuccess, ...rest }) => {
+	const { token } = useAdminAuth()
+	const { register, handleSubmit, formState, setValue } = useForm<IFormData>({
+		defaultValues: { name: "" },
+	})
+	const [errorText, setErrorText] = useState<string>()
+
+	const onSubmit = handleSubmit(async (data) => {
+		try {
+			await cityAddApi(data, token)
+			onSuccess && onSuccess()
+			rest.onClose()
+		} catch (err) {
+			setErrorText(err instanceof Error ? err.message : "Unknown Error")
+		}
+	})
+
+	return (
+		<DrawerForm
+			size={"sm"}
+			headingText="Add City"
+			onSubmit={onSubmit}
+			isSubmitting={formState.isSubmitting}
+			submitLabel={"Save"}
+			{...rest}
+		>
+			<Stack maxWidth={"sm"} marginX={"auto"}>
+				{/* Name */}
+				<FormControl>
+					<InputLabel label="City Name" />
+					<Input isRequired autoFocus {...register("name")} />
+				</FormControl>
+				{/* Name */}
+				<FormControl>
+					<InputLabel label="Select State" />
+					<Select<{ label: string; value: number }, false>
+						options={states.map((state) => ({
+							label: state.name,
+							value: state.id,
+						}))}
+						onChange={(selected) => {
+							selected && setValue("stateId", selected.value)
+						}}
+					/>
+				</FormControl>
+
+				{errorText && <ErrorText text={errorText} />}
+			</Stack>
+		</DrawerForm>
+	)
+}
