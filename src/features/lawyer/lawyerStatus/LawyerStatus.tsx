67b9cbcfaa0c935526993c8f16c4c09f48260fb2
Kunal Gosrani <iamkunalgosrani@gmail.com>
2022-02-16 23:49:05 +0530

    feat: lawyer status and proof upload

---
+++ src/features/lawyer/lawyerStatus/LawyerStatus.tsx
@@ -0,0 +1,59 @@
+import { Avatar, Box, Button, Center, Flex, Heading, Text } from "@chakra-ui/react"
+import _ from "lodash"
+import { FC, useEffect } from "react"
+import { Navigate, NavLink } from "react-router-dom"
+import { CenteredSpinner } from "../../shared/components/ui/CenterSpinner"
+import { useLawyerAuth } from "../useLawyerAuth"
+import { useLawyerStore } from "../useLawyerStore"
+import { LawyerProofs } from "./lawyerProofs/LawyerProofs"
+
+export const LawyerStatus: FC = () => {
+	const { token } = useLawyerAuth()
+	const { lawyer, fetchLawyer } = useLawyerStore()
+
+	useEffect(() => {
+		if (_.isUndefined(lawyer)) fetchLawyer({ token })
+	}, [fetchLawyer, token, lawyer])
+
+	if (_.isUndefined(lawyer)) return <CenteredSpinner />
+	if (!lawyer) return <Navigate to={"/lawyer/register"} />
+	if (lawyer.isVerified) return <Navigate to={"/lawyer"} />
+
+	return (
+		<Center>
+			<Box padding={10} maxW={"2xl"} textAlign="center">
+				<Heading size={"lg"}>Waiting for Verification</Heading>
+				<Text mt={1}>
+					It usually takes 3-4 business days for verification process. You might get
+					a telephone verification call on provided contact number.
+				</Text>
+				<Flex mt={8} p={4} gap={6} bgColor="gray.200" borderRadius={"lg"}>
+					<Flex direction={"column"}>
+						<Avatar size={"2xl"} />
+						<Button size={"xs"} mt={3} colorScheme="blackAlpha">
+							Update Profile Picture
+						</Button>
+					</Flex>
+					<Box textAlign={"left"}>
+						<Heading size={"md"}>{lawyer.name}</Heading>
+						<Text>{lawyer.description}</Text>
+						<Text>City: {lawyer.city.name}</Text>
+						<Text>{lawyer.address}</Text>
+						<Text>Contact: {lawyer.phone}</Text>
+						<Button size={"xs"} mt={1} colorScheme="blackAlpha">
+							Update Details
+						</Button>
+					</Box>
+				</Flex>
+				<Box mt={8} textAlign="left">
+					<LawyerProofs />
+				</Box>
+				<Box mt={8} textAlign="left">
+					<NavLink to="/logout">
+						<Button size={"sm"}>Logout</Button>
+					</NavLink>
+				</Box>
+			</Box>
+		</Center>
+	)
+}
--- src/features/lawyer/useLawyerAuth.ts
+++ src/features/lawyer/useLawyerAuth.ts
@@ -1,3 +1,4 @@
+import { useEffect } from "react"
 import { useNavigate } from "react-router-dom"
 import { AuthRole } from "../../utils/enums"
 import { storage } from "../../utils/storage"
@@ -7,8 +8,10 @@ export function useLawyerAuth() {
 	const navigate = useNavigate()
 	const authPayload = storage.getAuth()
 
-	if (!authPayload) navigate("/login")
-	if (authPayload?.role !== AuthRole.LAWYER) navigate("/login")
+	useEffect(() => {
+		if (!authPayload) navigate("/login")
+		if (authPayload?.role !== AuthRole.LAWYER) navigate("/login")
+	}, [authPayload, authPayload?.role, navigate])
 
 	return authPayload as IAuthPayload
 }
