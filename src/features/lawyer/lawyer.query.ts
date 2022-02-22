1108493d9b441d69aed77a29f97781071eecc776
Kunal Gosrani <iamkunalgosrani@gmail.com>
2022-02-23 00:16:53 +0530

    fix: lawyer isVerified auth

--- src/features/lawyer/lawyer.query.ts
+++ src/features/lawyer/lawyer.query.ts
@@ -1,5 +1,6 @@
 import { useCallback } from "react"
 import { useQuery, useQueryClient } from "react-query"
+import { storage } from "../../utils/storage"
 import { ILawyer } from "./ILawyer"
 import { apiLawyerSelfGet } from "./lawyerSelfGet.api"
 
@@ -7,7 +8,19 @@ type IDataShape = ILawyer | null
 const queryKey = "lawyer"
 
 export function useLawyerQuery({ token }: { token: string }) {
-	return useQuery<IDataShape, Error>(queryKey, () => apiLawyerSelfGet({ token }))
+	const auth = storage.getAuth()
+
+	return useQuery<IDataShape, Error>(queryKey, async () => {
+		if (!auth) return null
+
+		const isVerified = auth.isVerified
+		const lawyer = await apiLawyerSelfGet({ token })
+
+		if (lawyer && !isVerified !== lawyer.isVerified) {
+			storage.setIsVerified(lawyer.isVerified)
+		}
+		return lawyer
+	})
 }
 
 export function useLawyerData() {
