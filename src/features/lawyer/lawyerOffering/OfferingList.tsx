3505ea6cffed5e7eb170b824f1844cd5aac4197a
Kunal Gosrani <iamkunalgosrani@gmail.com>
2022-02-25 13:49:04 +0530

    feat: lawyer offering list mobile responsive layout

--- src/features/lawyer/lawyerOffering/OfferingList.tsx
+++ src/features/lawyer/lawyerOffering/OfferingList.tsx
@@ -1,4 +1,16 @@
-import { Table, Tbody, Td, Th, Thead, Tr } from "@chakra-ui/react"
+import {
+	Box,
+	Heading,
+	Table,
+	Tbody,
+	Td,
+	Th,
+	Thead,
+	Tr,
+	Text,
+	Flex,
+	Button,
+} from "@chakra-ui/react"
 import { FC } from "react"
 import { formatInr } from "../../../utils/helpers"
 import { CenteredSpinner } from "../../shared/components/ui/CenterSpinner"
@@ -18,46 +30,95 @@ export const OfferingList: FC = () => {
 	if (isOfferingsLoading || !offerings) return <CenteredSpinner />
 
 	return (
-		<Table size="sm" fontSize={"lg"}>
-			<Thead>
-				<Tr>
-					<Th>Paper Type</Th>
-					<Th>Language</Th>
-					<Th isNumeric>Expected Duration</Th>
-					<Th isNumeric>Price (INR)</Th>
-					<Th isNumeric>Status</Th>
-					<Th isNumeric></Th>
-				</Tr>
-			</Thead>
-			<Tbody>
-				{offerings.map((offering) => (
-					<Tr
+		<Box>
+			{/** For Mobile */}
+			<Box display={{ sm: "none" }}>
+				{offerings?.map((offering) => (
+					<Box
 						key={offering.id}
-						cursor="pointer"
-						_hover={{ backgroundColor: "gray.100" }}
+						m={4}
+						p={4}
+						border="1px"
+						borderColor={"gray.300"}
+						borderRadius={"lg"}
 					>
-						<Td fontWeight={"semibold"}>{offering.paperType.name}</Td>
-						<Td>{offering.language.name}</Td>
-						<Td isNumeric>{offering.expectedTimeInHours} Hrs</Td>
-						<Td isNumeric>{formatInr(offering.price)}</Td>
-						<Td isNumeric>{offering.isAvailable ? "ACTIVE" : "DISABLED"}</Td>
-						<Td isNumeric onClick={(e) => e.stopPropagation()}>
-							<EditIconButton
+						<Heading size={"lg"}>{offering.paperType.name}</Heading>
+						<Text>Language: {offering.language.name}</Text>
+						<Text>
+							Price: <b>{formatInr(offering.price)} INR</b>
+						</Text>
+						<Text>Expected Duration: {offering.expectedTimeInHours} Hrs</Text>
+						<Text>Status: {offering.isAvailable ? "ACTIVE" : "DISABLED"}</Text>
+						<Flex gap={2} mt={2}>
+							<Button
+								size={"sm"}
+								variant={"outline"}
+								colorScheme="yellow"
 								onClick={() => {
 									setSelectedOfferingForUpdate(offering)
 									setIsDrawerOpen(true)
 								}}
-							/>
-							<DeleteIconButton
+							>
+								Edit
+							</Button>
+							<Button
+								size={"sm"}
+								variant={"outline"}
+								colorScheme="red"
 								onClick={() => {
 									setSelectedOfferingForDelete(offering)
 									setIsDeleteDialogOpen(true)
 								}}
-							/>
-						</Td>
-					</Tr>
+							>
+								Delete
+							</Button>
+						</Flex>
+					</Box>
 				))}
-			</Tbody>
-		</Table>
+			</Box>
+
+			{/** For Desktop  */}
+			<Table size="sm" fontSize={"lg"} display={{ base: "none", sm: "table" }}>
+				<Thead>
+					<Tr>
+						<Th>Paper Type</Th>
+						<Th>Language</Th>
+						<Th isNumeric>Expected Duration</Th>
+						<Th isNumeric>Price (INR)</Th>
+						<Th isNumeric>Status</Th>
+						<Th isNumeric></Th>
+					</Tr>
+				</Thead>
+				<Tbody>
+					{offerings.map((offering) => (
+						<Tr
+							key={offering.id}
+							cursor="pointer"
+							_hover={{ backgroundColor: "gray.100" }}
+						>
+							<Td fontWeight={"semibold"}>{offering.paperType.name}</Td>
+							<Td>{offering.language.name}</Td>
+							<Td isNumeric>{offering.expectedTimeInHours} Hrs</Td>
+							<Td isNumeric>{formatInr(offering.price)}</Td>
+							<Td isNumeric>{offering.isAvailable ? "ACTIVE" : "DISABLED"}</Td>
+							<Td isNumeric onClick={(e) => e.stopPropagation()}>
+								<EditIconButton
+									onClick={() => {
+										setSelectedOfferingForUpdate(offering)
+										setIsDrawerOpen(true)
+									}}
+								/>
+								<DeleteIconButton
+									onClick={() => {
+										setSelectedOfferingForDelete(offering)
+										setIsDeleteDialogOpen(true)
+									}}
+								/>
+							</Td>
+						</Tr>
+					))}
+				</Tbody>
+			</Table>
+		</Box>
 	)
 }
